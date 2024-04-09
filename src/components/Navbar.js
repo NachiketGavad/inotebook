bfb52c6229833bbcde68142263247899e55cc7d5
NachiketGavad <nachiketgavad@gmail.com>
2024-04-09 20:08:34 +0530

    useLocation Practice

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React,{useEffect} from "react";
+import { Link,useLocation } from "react-router-dom";
 
 const Navbar = () => {
+    let location = useLocation();
+    useEffect(() => {
+        console.log(location.pathname);
+      }, [location]);
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             iNotebook
@@ -23,12 +28,12 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link className={`nav-link ${location.pathname==='/'?'active':""}`} aria-current="page" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/About">
+                <Link className={`nav-link ${location.pathname==='/About'?'active':""}`}to="/About">
                   About
                 </Link>
               </li>
